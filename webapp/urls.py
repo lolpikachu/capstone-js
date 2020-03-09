5ac860055e5c036f03ca51f8eda2425f67d5d8f0
Pikachu <anzhelikadarijchuk@gmail.com>
2020-03-09 18:47:57 +0200

    settings urls.py added static

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
