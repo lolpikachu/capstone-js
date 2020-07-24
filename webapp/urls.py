b235f08a581c301228c5b79750af70bbdf0b5ceb
Pikachu <anzhelikadarijchuk@gmail.com>
2020-07-24 15:47:01 +0200

    'theme change'

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ru', views.index_ru, name='index_ru'),
+    # path('ru', views.index_ru, name='index_ru'),
     path('portfolio', views.portfolio, name='portfolio'),
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services'),
+    path('about', views.about, name='about'),
     path('portfolio/example-1', views.example_1, name='example_1'),
 ]
+
 
--- webapp/views.py
+++ webapp/views.py
@@ -4,13 +4,19 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'webapp/index3.html')
+    return render(request, 'webapp/index.html')
 
-def index_ru(request):
-    return render(request, 'webapp/index_ru.html')
+def about(request):
+    return render(request, 'webapp/about.html')
 
 def portfolio(request):
     return render(request, 'webapp/portfolio.html')
+
+def contact(request):
+    return render(request, 'webapp/contact.html')
+
+def services(request):
+    return render(request, 'webapp/services.html')        
 
 def example_1(request):
     return render(request, 'webapp/portfolio/example_1.html')             
