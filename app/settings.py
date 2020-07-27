cb0b50f974f947aab2dd200f5638fbc88cbd26bb
Pikachu <anzhelikadarijchuk@gmail.com>
2020-07-27 13:46:19 +0200

    theme changes

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i*wwenlw7-fxxq7@q%pdv4&yo!x%n!km!uf9f6%rf54u=82@6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '95.217.155.245', 'ovrax.com']
 
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['webapp/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- webapp/views.py
+++ webapp/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'webapp/index.html')
 
+def index_ru(request):
+    return render(request, 'webapp/index_ru.html')
+
 def about(request):
     return render(request, 'webapp/about.html')
 
@@ -15,12 +18,25 @@ def portfolio(request):
 def contact(request):
     return render(request, 'webapp/contact.html')
 
-def services(request):
-    return render(request, 'webapp/services.html')        
+# def services(request):
+#     return render(request, 'webapp/services.html')        
 
 def example_1(request):
     return render(request, 'webapp/portfolio/example_1.html')             
 
+def example_2(request):
+    return render(request, 'webapp/portfolio/example_2.html')    
 
+def example_3(request):
+    return render(request, 'webapp/portfolio/example_3.html')    
+
+def example_4(request):
+    return render(request, 'webapp/portfolio/example_4.html')    
+
+def example_5(request):
+    return render(request, 'webapp/portfolio/example_5.html') 
+
+def example_6(request):
+    return render(request, 'webapp/portfolio/example_6.html')    
 
 
